ea97c9bd30e7d97d98837c5d05a4d6653a9cb2b4
Marco2145 <magt12265@gmail.com>
2025-09-27 16:56:56 -0600

    lesson 111 - Section 09

---
+++ 04-country-app/src/app/country/components/country-search-input/country-search-input.component.ts
@@ -0,0 +1,12 @@
+import { Component, input, inputBinding, output } from '@angular/core';
+
+@Component({
+  selector: 'country-search-input',
+  imports: [],
+  templateUrl: './country-search-input.component.html',
+})
+export class CountrySearchInputComponent {
+  placeholder = input<string>('Buscar');
+
+  value = output<string>();
+}
