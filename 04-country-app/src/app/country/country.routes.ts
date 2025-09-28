c14e94919f27eff2ec71322afbbacaa097142bbc
Marco2145 <magt12265@gmail.com>
2025-09-28 13:28:53 -0600

    lesson 114 - Section 09
    Fin de la sección, se muestran todas las vistas, aunque no hay funcionalidad

--- 04-country-app/src/app/country/country.routes.ts
+++ 04-country-app/src/app/country/country.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { CountryLayoutComponent } from './layouts/CountryLayout/countryLayout.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 export const countryRoutes: Routes = [
   {
@@ -18,7 +20,11 @@ export const countryRoutes: Routes = [
       },
       {
         path: 'by-region',
-        component: ByCapitalPageComponent,
+        component: ByRegionPageComponent,
+      },
+      {
+        path: 'by/:code',
+        component: CountryPageComponent,
       },
       {
         path: '**',
