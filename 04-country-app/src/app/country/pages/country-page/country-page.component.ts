142c5eb10a89bf20a36b32c49e17d915fac0e757
Marco2145 <magt12265@gmail.com>
2025-09-29 18:51:42 -0600

    Lesson 132 - Section 10
    Fin de sección
    Se muestran países al buscarlo, ya sea por capital o por nombre,
    Ventana de más detalles muestra un poco de información general

--- 04-country-app/src/app/country/pages/country-page/country-page.component.ts
+++ 04-country-app/src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { of, delay, catchError } from 'rxjs';
+import { CountryService } from '../../services/country.service';
+import { NotFoundComponent } from '../../../shared/components/not-found/not-found.component';
+import { CountryInformationComponent } from './country-information/country-information.component';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [NotFoundComponent, CountryInformationComponent],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent {}
+export class CountryPageComponent {
+  countryCode = inject(ActivatedRoute).snapshot.params['code'];
+  countryService = inject(CountryService);
+
+  // Con rxResource se trabaja con un observable
+  countryResource = rxResource({
+    params: () => ({ query: this.countryCode }),
+    stream: ({ params }) => {
+      if (!params.query) return of();
+
+      delay(500);
+      return this.countryService.searchByAlphaCode(params.query).pipe(
+        // *Nota: No funciona el mostrar error en la tabla, countryService.error() no da nada
+        catchError((error) => {
+          console.error(error);
+          return of();
+        })
+      );
+    },
+  });
+}
