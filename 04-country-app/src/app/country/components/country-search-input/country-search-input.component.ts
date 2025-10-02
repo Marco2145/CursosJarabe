e3f379056ce80d694d3d7cb0239e6f55f9c4f58c
Marco2145 <magt12265@gmail.com>
2025-10-01 18:15:15 -0600

    Lesson 139 - Section 11
    Implementar caché de busqueda y debounce

--- 04-country-app/src/app/country/components/country-search-input/country-search-input.component.ts
+++ 04-country-app/src/app/country/components/country-search-input/country-search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, inputBinding, output } from '@angular/core';
+import { Component, effect, input, inputBinding, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -7,6 +7,20 @@ import { Component, input, inputBinding, output } from '@angular/core';
 })
 export class CountrySearchInputComponent {
   placeholder = input<string>('Buscar');
-
   value = output<string>();
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleaup) => {
+    // Muy importante este paso -> la señal indica a angular que requiere lanzar el efecto
+    const value = this.inputValue();
+
+    const myTimeout = setTimeout(() => {
+      this.value.emit(value);
+    }, 500);
+
+    onCleaup(() => {
+      clearTimeout(myTimeout);
+    });
+  });
 }
