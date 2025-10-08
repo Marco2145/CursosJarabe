5c6f12b0596890d03611de4cf3fa5fcc043f2b6c
Marco2145 <magt12265@gmail.com>
2025-10-08 00:16:07 -0600

    lesson 155 - Section 12
    Set locale para japones, inglés, y español

--- 05-pipes-app/src/app/app.config.ts
+++ 05-pipes-app/src/app/app.config.ts
@@ -1,12 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+// Para que angular sepa qué significa el "es" en locale
+import localEs from '@angular/common/locales/es-MX';
+import localJa from '@angular/common/locales/ja';
+import { LocaleService } from './services/locale.service';
+registerLocaleData(localEs, 'es');
+registerLocaleData(localJa, 'ja');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    {
+      // Para cambiar el locale ID
+      provide: LOCALE_ID,
+      // useValue: 'es',
+      deps: [LocaleService],
+      useFactory: (localeService: LocaleService) => localeService.getLocale,
+    },
+  ],
 };
--- 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,14 +1,42 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+  LOCALE_ID,
+  signal,
+} from '@angular/core';
+import { availableLocale, LocaleService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class BasicPageComponent {
-  nameLower = signal('layla');
-  nameUpper = signal('LAYLA');
-  fullName = signal('laYLa ');
+  localeService = inject(LocaleService);
+  currentLocale = signal(inject(LOCALE_ID));
+
+  nameLower = signal('harmony');
+  nameUpper = signal('HARMONY');
+  fullName = signal('hArMoNY nO SuRnAmE ');
+
+  myDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.myDate.set(new Date());
+      // console.log('tick')
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  });
+
+  changeLocale(locale: availableLocale) {
+    this.localeService.changeLocal(locale);
+  }
 }
