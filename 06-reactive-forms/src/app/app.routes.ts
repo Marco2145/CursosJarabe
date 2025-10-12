811c70163dbb65b41e92c4def1b2878041e15fc0
Marco2145 <magt12265@gmail.com>
2025-10-12 16:00:22 -0600

    Lesson 187 - Section 14
    Formularios reactivos, se implementó una clase de utilidades que manda los mensajes de error para que puedan ser vistos por el usuario,

---
+++ 06-reactive-forms/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () =>
+      import('./reactive/reactive.routes').then((module) => module.reactiveRoutes),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'), // Este tiene authRoutes exportado por default
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes').then((module) => module.countryRoutes),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive',
+  },
+];
