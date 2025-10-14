bf206f19b7f82c02b873e9895b0c7f91c82823a8
Marco2145 <magt12265@gmail.com>
2025-10-13 18:46:31 -0600

    Lesson 201 - Section 15
    Modulo de auth, con validaciones personalizadas

--- 06-reactive-forms/src/app/utils/form-utils.ts
+++ 06-reactive-forms/src/app/utils/form-utils.ts
@@ -1,7 +1,24 @@
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+
+async function sleep() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2500);
+  });
+}
 
 export class FormUtils {
   // Expresiones regulares
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
   static getTextError(errors: ValidationErrors): string | null {
     for (const key of Object.keys(errors)) {
@@ -12,6 +29,20 @@ export class FormUtils {
           return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
         case 'min':
           return `Valor mínimo de ${errors['min'].min}`;
+        case 'email':
+          return 'Debe ingresar un correo válido';
+        case 'emailTaken':
+          return 'El correo ya está siendo usado por otro usuario';
+        case 'noStrider':
+          return 'El nombre de usuario "Strider" no está permitido';
+        case 'pattern':
+          if (errors['pattern'].requiredPattern == this.emailPattern) {
+            return 'Debe ingresar un correo válido';
+          } else {
+            return '';
+          }
+        default:
+          return key;
       }
     }
     return null;
@@ -41,5 +72,46 @@ export class FormUtils {
     const errors = formArray.controls[index].errors ?? {};
 
     return FormUtils.getTextError(errors);
+  }
+
+  // Para hacer las validaciones se necesita una funcion que retorne una funcion
+  // Recibe como argumentos el formGroup de tipo AbstractControl, y de ahí podemos empezar a hacer validaciones
+  // Esa funcion retornada debe retornar null si todo está bien, o un obj descriptivo (ValidationErrors) si algo salió mal
+  static areFieldsEqual(field1: string, field2: string, errorName: string): ValidatorFn {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { [errorName]: true };
+    };
+  }
+
+  static async checkingServerResponse(control: AbstractControl): Promise<ValidationErrors | null> {
+    console.log('Validando contra servidor...');
+    await sleep();
+    const formValue = control.value;
+
+    if (formValue == 'hola@mundo.com') {
+      return { emailTaken: true };
+    } else {
+      return null;
+    }
+  }
+
+  // Verificar que el nombre de usuario no sea strider
+  // Mi solución ?
+  // static notStrider(): ValidatorFn {
+  //   return (control: AbstractControl) => {
+  //     const username = control.value;
+
+  //     return username === 'strider' ? { noStrider: true } : null;
+  //   };
+  // }
+
+  // Solucion propuesta en video
+  static notStrider(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+
+    return value === 'strider' ? { noStrider: true } : null;
   }
 }
