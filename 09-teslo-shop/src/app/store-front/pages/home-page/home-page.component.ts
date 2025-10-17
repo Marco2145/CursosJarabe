b0ee8584d9dce036218449234bb97b63d02d870d
Marco2145 <magt12265@gmail.com>
2025-10-17 17:38:16 -0600

    Lesson 251 - Section 18
    Se muestra los productos obtenidos del backend, con imagen, desc, y demás

--- 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ProductsService } from '@products/services/products.service';
 import { ProductCardComponent } from '@store-front/components/product-card/product-card.component';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { tap } from 'rxjs';
+import { ProductsResponse } from '@products/interfaces/product.interface';
 
 @Component({
   selector: 'app-home-page',
   imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({});
+    },
+  });
+}
