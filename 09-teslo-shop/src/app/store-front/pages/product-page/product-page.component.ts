ebe98a42b22d3b146d25d5b8551c24b00e6daac9
Marco2145 <magt12265@gmail.com>
2025-10-17 19:51:55 -0600

    Lesson 254 - Section 18
    Fin de sección:
    Pagina de productos por género.
    Carrusel en la pagina de productos

--- 09-teslo-shop/src/app/store-front/pages/product-page/product-page.component.ts
+++ 09-teslo-shop/src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { tap } from 'rxjs';
+import { ProductCarouselComponent } from '@store-front/components/product-carousel/product-carousel.component';
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent {}
+export class ProductPageComponent {
+  private activatedRoute = inject(ActivatedRoute);
+  private productsService = inject(ProductsService);
+
+  // ? Este es dinámico, pero en la clase se propuso uno estático, así que se cambió
+  // productIdSlug = signal('');
+  // constructor() {
+  //   this.activatedRoute.params.subscribe((params) => {
+  //     this.productIdSlug.set(params['idSlug']);
+  //   });
+  // }
+
+  productIdSlug: string = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = rxResource({
+    params: () => ({ id: this.productIdSlug }),
+    stream: ({ params }) => {
+      return this.productsService.getProductByIdSlug(params.id);
+    },
+  });
+}
