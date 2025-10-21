49c427811ffd1835fa8f7dc739650e30c8f98b15
Marco2145 <magt12265@gmail.com>
2025-10-20 19:21:26 -0600

    Lesson 277 - Section 20
    Login funciona, guarda sesión en local storage, y la recupera

--- 09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
+++ 09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+  router = inject(Router);
+
+  authService = inject(AuthService);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.showError();
+      return;
+    }
+
+    const { email = '', password = '' } = this.loginForm.value;
+    console.log({ email, password });
+
+    this.authService.login(email!, password!).subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigateByUrl('/', {
+          replaceUrl: true,
+        });
+        return;
+      }
+      this.showError();
+    });
+  }
+
+  showError() {
+    this.hasError.set(true);
+    setTimeout(() => {
+      this.hasError.set(false);
+    }, 6000);
+  }
+
+  // Check authentication
+  // Registro
+  // Logout
+}
