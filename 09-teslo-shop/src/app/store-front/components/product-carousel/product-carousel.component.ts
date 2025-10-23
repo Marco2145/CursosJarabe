bb23f5621779d1634c840a972146ae8b9b541640
Marco2145 <magt12265@gmail.com>
2025-10-22 20:47:19 -0600

    Lesson 310 - Section 22
    Carga de archivos para actualización y creación de producto

--- 09-teslo-shop/src/app/store-front/components/product-carousel/product-carousel.component.ts
+++ 09-teslo-shop/src/app/store-front/components/product-carousel/product-carousel.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, ElementRef, input, viewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  input,
+  OnChanges,
+  SimpleChanges,
+  viewChild,
+} from '@angular/core';
 
 // import Swiper JS
 import Swiper from 'swiper';
@@ -21,16 +29,29 @@ import { ProductImagePipe } from '@products/pipes/product-image.pipe';
    }
   `,
 })
-export class ProductCarouselComponent implements AfterViewInit {
+export class ProductCarouselComponent implements AfterViewInit, OnChanges {
   images = input.required<string[]>();
-
   swiperDiv = viewChild.required<ElementRef>('swiperDiv');
+  swiper: Swiper | null = null;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['images'].firstChange) return;
+
+    if (!this.swiper) return;
+
+    this.swiper.destroy(true, true);
+    this.swiperInit();
+  }
 
   ngAfterViewInit(): void {
+    this.swiperInit();
+  }
+
+  swiperInit() {
     const element = this.swiperDiv().nativeElement;
     if (!element) return;
 
-    const swiper = new Swiper(element, {
+    this.swiper = new Swiper(element, {
       // Optional parameters
       direction: 'horizontal',
       loop: true,
