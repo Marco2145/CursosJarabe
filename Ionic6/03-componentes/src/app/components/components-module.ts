98660806691f0f7d5d2dec87dd60d695216bdd4d
Marco2145 <magt12265@gmail.com>
2025-10-25 17:17:50 -0600

    Lesson 40 - Section 04
    Fin de sección
    Se hizo una app simple con dos botones que llevan a páginas distintas

---
+++ Ionic6/03-componentes/src/app/components/components-module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Segun el video debería ser así, pero ahora los componentes son standalone
+/** 
+  @NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent],
+})
+ **/
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class ComponentsModule {}
